c33f1b727945e62a9c58bc60b3e2983c2277ae3c
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2024-04-11 16:58:55 +0300

    auth

--- api/user.js
+++ api/user.js
@@ -1,23 +1,24 @@
-import firebase from '@react-native-firebase/app'
+import {firebase} from '@react-native-firebase/app'
+import { initializeApp } from "firebase/app";
 import auth from "@react-native-firebase/auth"
 import { useEffect } from 'react';
 // asenktron bir işlemin başlatıldıktan sonra sonuç beklenmeden diğer işlemlerin devam etmesine izin veren bir programlama kavramıdır.
 //Örneğin, ağ isteği yapma, dosya okuma/yazma, zamanlayıcı kullanma gibi işlemler genellikle asenkron olarak gerçekleştirilir.
 // Ancak bazen bir sonraki sayfaya geçmesini bekledğimizde  doğrulamanın onaylanması için "await" işlemi yapısı kullanılır.
 //Asenkron bir işlevi çalıştırmak için await anahtar kelimesini kullanabilirsiniz. Bu, işlemin tamamlanmasını bekler ve ardından işlemin sonucunu bir değişkene atar.
-const firebaseConfig = {
-    // Your actual Firebase project configuration goes here
-    // (API key, auth domain, project ID, etc.)
-    // apiKey: "AIzaSyCdmzBE6NjD2FXTSXOifRVA638YIRPalUA",
-    // project_number: "940633775110",
-    // project_id: "donation-application-f3c59",
-    // storage_bucket: "donation-application-f3c59.appspot.com"  ,   
-}
+// const firebaseConfig = {
+//     // Your actual Firebase project configuration goes here
+//     // (API key, auth domain, project ID, etc.)
+//     // apiKey: "AIzaSyCdmzBE6NjD2FXTSXOifRVA638YIRPalUA",
+//     // project_number: "940633775110",
+//     // project_id: "donation-application-f3c59",
+//     // storage_bucket: "donation-application-f3c59.appspot.com"  ,   
+// }
   
-  // Initialize Firebase app only if it hasn't been initialized yet
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+//   // Initialize Firebase app only if it hasn't been initialized yet
+//   if (!firebase.apps.length) {
+//     firebase.initializeApp(firebaseConfig);
+//   }
   
 export const createUser = async (fullName, email, password) => { 
     try { 
